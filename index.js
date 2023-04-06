3992ee6ef73fa3be122fb0a93809021b5d9afdd1
teddyfav <anwulikaogbogu@yahoo.com>
2023-04-06 10:42:28 +0100

    anwulika

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-let today = "far gone";
-let anuulit = "fight all you car!s";
+function test_prime(n) {
+
+    if (n === 1) {
+        return false;
+    }
+    else if (n === 2) {
+        return true;
+    } else {
+        for (var x = 2; x < n; x++) {
+            if (n % x === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+console.log(test_prime(34));
